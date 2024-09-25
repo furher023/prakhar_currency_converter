f7c2e1abf1fffd0ade81cc60283741447eda1d8d
Prakhar Vijay <prakhar@binaryveda.com>
2024-09-25 12:43:29 +0530

    Added API key auth on routes and added sonarcube support

--- src/bill/bill.controller.ts
+++ src/bill/bill.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { BillService } from './bill.service';
 import { CalculateBillRequestDto } from './dto/calculate-billl-request-body.dto';
 import { CalculateBillResponseDto } from './dto/calculate-bill-response.dto';
+import { ApiKeyGuard } from 'src/auth/api-key.guard';
 
 @Controller('api')
 export class BillController {
     constructor(private billService: BillService){}
 
     @Post('calculate')
+    @UseGuards(ApiKeyGuard) 
     async calculateBill(@Body() billDetails: CalculateBillRequestDto): Promise<CalculateBillResponseDto>{
         return this.billService.calculate(billDetails);
     }
