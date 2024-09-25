5dd8860458d62d18faedb4a0b039ac49028f4ac6
Prakhar Vijay <prakhar@binaryveda.com>
2024-09-25 10:24:57 +0530

    defined the api-key guard for unprotected routes

---
+++ src/auth/api-key.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+    constructor(private configService: ConfigService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers['x-api-key'];
+    if (!apiKey || apiKey !== this.configService.get('API_KEY')) {
+      throw new UnauthorizedException('Invalid API key');
+    }
+
+    return true;
+  }
+}
